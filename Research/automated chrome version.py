38ee1546365c70885de9c63196d83292cf9d617d
rtoum <r.touma@live.de>
2022-03-17 11:08:39 +0100

    testing automated chrome version

---
+++ Research/automated chrome version.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import pandas as pd
+
+# Copy path to Chrome
+browser = webdriver.Chrome("D:/Ricarda/Dokumente/Studium/Hochschule Düsseldorf/4. Semester/"
+                           "Thesis/Code/Web_Crawler/chromedriver_win32/chromedriver.exe")
+
+# Determine a website
+url = "https://www.w3schools.com/html/html_tables.asp"
+# Open the website in the automated chrome version
+browser.get(url)
+# Read the Text on the website
+text_website = browser.page_source
+df = pd.read_html(text_website)
+print(df)
