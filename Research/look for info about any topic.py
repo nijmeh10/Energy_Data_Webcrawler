c363ee6e8f7603de54724be86a2f9bddc88c8c57
rtoum <r.touma@live.de>
2022-03-25 15:23:36 +0100

    tested ways to crawl infos about a specific topic

--- Research/look for info about any topic.py
+++ Research/look for info about any topic.py
@@ -79,26 +79,39 @@ class Crawler:
                 content = Content(topic, title, body, url)
                 content.print()
 
+    def parse(self, site, url):
+        """
+        Extract content from a given page URL
+        """
+        bs = self.getPage(url)
+        if bs is not None:
+            title = self.safeGet(bs, site.titleTag)
+            body = self.safeGet(bs, site.bodyTag)
+            if title != '' and body != '':
+                content = Content(url, title, body)
+                content.print()
 
 crawler = Crawler()
 
 
 
-siteData = [
-    ['O\'Reilly Media', 'http://oreilly.com', 'https://ssearch.oreilly.com/?q=',
-        'article.product-result', 'p.title a', True, 'h1', 'section#product-description'],
-    ['Reuters', 'http://reuters.com', 'http://www.reuters.com/search/news?blob=', 'div.search-result-content',
-        'h3.search-result-title a', False, 'h1', 'div.StandardArticleBody_body_1gnLA'],
-    ['Brookings', 'http://www.brookings.edu', 'https://www.brookings.edu/search/?s=',
-        'div.list-content article', 'h4.title a', True, 'h1', 'div.post-body']
-]
-sites = []
-for row in siteData:
-    sites.append(Website(row[0], row[1], row[2],
-                         row[3], row[4], row[5], row[6], row[7]))
 
-topics = ['python', 'data science']
-for topic in topics:
-    print('GETTING INFO ABOUT: ' + topic)
-    for targetSite in sites:
-        crawler.search(topic, targetSite)
+
+siteData = [
+    ['O\'Reilly Media', 'http://oreilly.com','h1', 'section#product-description'],
+    ['Reuters', 'http://reuters.com', 'h1','div.StandardArticleBody_body_1gnLA'],
+    ['Brookings', 'http://www.brookings.edu', 'h1', 'div.post-body'],
+    ['New York Times', 'http://nytimes.com','h1', 'p.story-content']]
+
+websites = []
+for row in siteData:
+    websites.append(Website(row[0], row[1], row[2], row[3]))
+
+crawler.parse(websites[0], 'http://shop.oreilly.com/product/'\
+                           '0636920028154.do')
+crawler.parse(websites[1], 'http://www.reuters.com/article/'\
+                           'us-usa-epa-pruitt-idUSKBN19W2D0')
+crawler.parse(websites[2], 'https://www.brookings.edu/blog/'\
+                           'techtank/2016/03/01/idea-to-retire-old-methods-of-policy-education/')
+crawler.parse(websites[3], 'https://www.nytimes.com/2018/01/'\
+                           '28/business/energy-environment/oil-boom.html')
